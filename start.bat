2be5f7a27896b64845d3ba00209ee27f75012f4d
SnippyRO <81987817+SnippyRP@users.noreply.github.com>
2021-04-05 17:52:26 -0700

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,1 @@
+python superfinder.py
---
+++ superfinder.py
@@ -0,0 +1,30 @@
+count = 0
+con = 1
+import time
+import os
+import pythonroblox
+import requests
+interger = 1
+print("To start group scanner, press any key")
+os.system("Pause")
+while count < 2:
+    try:
+        groups = pythonroblox.Groups()
+        result = groups.search_id(con)
+        con = con + int(interger)
+        groupmem = result.member_count
+        groupname = result.name
+        groupid = result.id
+        groupown = result.owner_name
+        args = "[member_count:", groupmem, ", group_name:", groupname, ", group_id:", groupid, ", group_owner:", groupown, "]"
+        print(args)
+    except Exception as ex:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        if message.find("HTTP404"):
+            print("Connection timed out")
+        if message.find("Subscriptable"):
+            con = con + int(interger)
+            print("Error while loading, timed out for 1 second. The group ID is: ", con-int(1))
+        time.sleep(1)
+    time.sleep(0.1)
